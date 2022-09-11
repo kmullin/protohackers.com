267a31692d67203b50d2945d656e4dff50a0b629
Kevin Mullin <kevin@kpmullin.com>
2022-09-10 22:42:49 -0700

    working from http handler example

--- 2/main.go
+++ 2/main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	server.TCP(handler)
+	server.TCP(server.HandlerFunc(handler))
 }
 
 func handler(conn net.Conn) {
--- server/tcp.go
+++ server/tcp.go
@@ -5,9 +5,17 @@ import (
 	"net"
 )
 
-type HandleFunc func(net.Conn)
+type HandlerFunc func(net.Conn)
 
-func TCP(handler HandleFunc) {
+func (f HandlerFunc) HandleTCP(conn net.Conn) {
+	f(conn)
+}
+
+type Handler interface {
+	HandleTCP(net.Conn)
+}
+
+func TCP(h Handler) {
 	ln, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		log.Fatalf("unable to listen: %v", err)
@@ -20,6 +28,6 @@ func TCP(handler HandleFunc) {
 			log.Printf("accept err: %v", err)
 			continue
 		}
-		go handler(conn)
+		go h.HandleTCP(conn)
 	}
 }
