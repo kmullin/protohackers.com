34a0f0e9d3af7e5113c9397faeea81e649e17545
Kevin Mullin <kevin@kpmullin.com>
2022-09-25 00:05:03 -0700

    test

--- 3/chat/session_test.go
+++ 3/chat/session_test.go
@@ -8,14 +8,30 @@ import (
 )
 
 func TestSession(t *testing.T) {
-	client, server := test.Conn(t)
+	cases := []struct {
+		Name    string
+		Payload string
+		Valid   bool
+	}{
+		{"missing newline", "foo", false},
+		{"valid", "foo\n", true},
+		{"invalid single char", "\n", false},
+	}
+	for _, c := range cases {
+		t.Run(c.Name, func(t *testing.T) {
+			client, server := test.Conn(t)
 
-	// write some valid name without a trailing newline '\n'
-	fmt.Fprintf(client, "foo")
-	client.Close()
+			// write some valid name without a trailing newline '\n'
+			fmt.Fprintf(client, c.Payload)
+			client.Close()
 
-	session, err := NewSession(server)
-	if err == nil {
-		t.Fatalf("expected err, got session user: %+v", session.User)
+			session, err := NewSession(server)
+			if c.Valid && err != nil {
+				t.Fatalf("expected nil, got err: %v", err)
+			}
+			if !c.Valid && err == nil {
+				t.Fatalf("expected err, got session user: %+v", session.User)
+			}
+		})
 	}
 }
