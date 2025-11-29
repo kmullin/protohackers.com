0a146cea8e69b5e60af5b83d385fc56825450ae4
Kevin Mullin <kevin@kpmullin.com>
2025-11-28 16:19:23 -0800

    working on problem 6: speed daemon

---
+++ 6/server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net"
+	"time"
+
+	"github.com/rs/zerolog"
+)
+
+type Server struct {
+	logger zerolog.Logger
+}
+
+type Camera struct {
+	Road       string
+	Location   string
+	SpeedLimit string
+}
+
+type Picture struct {
+	Plate     string
+	Timestamp time.Time
+}
+
+func (s *Server) HandleTCP(conn net.Conn) {
+	// tear down client connection after disconnect
+	defer conn.Close()
+
+	s.logger.Info().Stringer("client", conn.RemoteAddr()).Msg("disconnected")
+}
