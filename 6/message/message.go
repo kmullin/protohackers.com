7871c1157023b6a60017ca64f4a7a615c8e96642
Kevin Mullin <kevin@kpmullin.com>
2025-11-28 21:57:50 -0800

    add ticket message

--- 6/message/message.go
+++ 6/message/message.go
@@ -11,6 +11,11 @@ import (
 
 var ByteOrder = binary.BigEndian
 
+var ErrLargeMsg = errors.New("msg is too large")
+
+// MaxMsgLen is the maximum decimal value of a uint8
+const MaxMsgLen = int(^uint8(0))
+
 type Message interface {
 	encoding.BinaryMarshaler
 }
@@ -18,11 +23,6 @@ type Message interface {
 type Error struct {
 	Msg string
 }
-
-var ErrLargeMsg = errors.New("msg is too large")
-
-// MaxMsgLen is the maximum decimal value of a uint8
-const MaxMsgLen = int(^uint8(0))
 
 func (e Error) MarshalBinary() ([]byte, error) {
 	var buf bytes.Buffer
@@ -45,6 +45,57 @@ func (e Error) MarshalBinary() ([]byte, error) {
 
 	_, err = buf.WriteString(e.Msg)
 	if err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+type Ticket struct {
+	Plate      string
+	Road       int
+	Mile1      int
+	Timestamp1 time.Time
+	Mile2      int
+	Timestamp2 time.Time
+	Speed      int
+}
+
+func (t Ticket) MarshalBinary() ([]byte, error) {
+	var buf bytes.Buffer
+
+	msg := struct {
+		Type   uint8
+		StrLen uint8
+	}{
+		0x21, uint8(len(t.Plate)),
+	}
+
+	if err := binary.Write(&buf, ByteOrder, &msg); err != nil {
+		return nil, err
+	}
+
+	if _, err := buf.WriteString(t.Plate); err != nil {
+		return nil, err
+	}
+
+	msg2 := struct {
+		Road       uint16
+		Mile1      uint16
+		Timestamp1 uint32
+		Mile2      uint16
+		Timestamp2 uint32
+		Speed      uint16
+	}{
+		uint16(t.Road),
+		uint16(t.Mile1),
+		uint32(t.Timestamp1.Unix()),
+		uint16(t.Mile2),
+		uint32(t.Timestamp2.Unix()),
+		uint16(t.Speed),
+	}
+
+	if err := binary.Write(&buf, ByteOrder, &msg2); err != nil {
 		return nil, err
 	}
 
--- 6/message/message_test.go
+++ 6/message/message_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -33,6 +34,46 @@ func TestMsgLength(t *testing.T) {
 	e := Error{RandStringRunes(256)}
 	_, err := e.MarshalBinary()
 	assert.Error(t, err)
+}
+
+func TestTicketMsg(t *testing.T) {
+	cases := []struct {
+		Ticket   Ticket
+		Expected []byte
+	}{
+		{
+			Ticket{
+				Plate:      "UN1X",
+				Road:       66,
+				Mile1:      100,
+				Timestamp1: time.Unix(123456, 0).UTC(),
+				Mile2:      110,
+				Timestamp2: time.Unix(123816, 0).UTC(),
+				Speed:      10000,
+			}, []byte{0x21, 0x04, 0x55, 0x4e, 0x31, 0x58, 0x00, 0x42, 0x00, 0x64, 0x00, 0x01, 0xe2, 0x40, 0x00, 0x6e, 0x00, 0x01, 0xe3, 0xa8, 0x27, 0x10},
+		},
+
+		{
+			Ticket{
+				Plate:      "RE05BKG",
+				Road:       368,
+				Mile1:      1234,
+				Timestamp1: time.Unix(1000000, 0).UTC(),
+				Mile2:      1235,
+				Timestamp2: time.Unix(1000060, 0).UTC(),
+				Speed:      6000,
+			},
+			[]byte{0x21, 0x07, 0x52, 0x45, 0x30, 0x35, 0x42, 0x4b, 0x47, 0x01, 0x70, 0x04, 0xd2, 0x00, 0x0f, 0x42, 0x40, 0x04, 0xd3, 0x00, 0x0f, 0x42, 0x7c, 0x17, 0x70},
+		},
+	}
+
+	for i, tc := range cases {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			b, err := tc.Ticket.MarshalBinary()
+			assert.Nil(t, err)
+			assert.Equal(t, tc.Expected, b)
+		})
+	}
 }
 
 func TestClientMsg(t *testing.T) {
