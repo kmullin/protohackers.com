6a9ce7716736deffdde4648152293ded6e450f09
Kevin Mullin <kevin@kpmullin.com>
2025-11-28 21:24:45 -0800

    read plate msg

--- 6/message/message.go
+++ 6/message/message.go
@@ -5,6 +5,8 @@ import (
 	"encoding"
 	"encoding/binary"
 	"errors"
+	"io"
+	"time"
 )
 
 var ByteOrder = binary.BigEndian
@@ -47,4 +49,49 @@ func (e Error) MarshalBinary() ([]byte, error) {
 	}
 
 	return buf.Bytes(), nil
+}
+
+type Plate struct {
+	Plate     string
+	Timestamp time.Time
+}
+
+func (p Plate) MarshalBinary() ([]byte, error) {
+	return nil, nil
+}
+
+func New(r io.Reader) (Message, error) {
+	var t uint8
+
+	// find out which message we receive
+	err := binary.Read(r, ByteOrder, &t)
+	if err != nil {
+		return nil, err
+	}
+
+	switch t {
+	case 0x20: // Plate
+		var l uint8
+
+		if err := binary.Read(r, ByteOrder, &l); err != nil {
+			return nil, err
+		}
+
+		buf := make([]byte, l)
+		if err := binary.Read(r, ByteOrder, &buf); err != nil {
+			return nil, err
+		}
+
+		var ts uint32
+		if err := binary.Read(r, ByteOrder, &ts); err != nil {
+			return nil, err
+		}
+
+		return &Plate{Plate: string(buf), Timestamp: time.Unix(int64(ts), 0).UTC()}, nil
+	case 0x40:
+	default:
+		return nil, errors.New("unknown message received")
+	}
+
+	return nil, nil
 }
--- 6/message/message_test.go
+++ 6/message/message_test.go
@@ -1,6 +1,7 @@
 package message
 
 import (
+	"bytes"
 	"fmt"
 	"math/rand"
 	"testing"
@@ -32,6 +33,22 @@ func TestMsgLength(t *testing.T) {
 	e := Error{RandStringRunes(256)}
 	_, err := e.MarshalBinary()
 	assert.Error(t, err)
+}
+
+func TestClientMsg(t *testing.T) {
+	// Plate msg
+	r := bytes.NewReader([]byte{0x20, 0x04, 0x55, 0x4e, 0x31, 0x58, 0x00, 0x00, 0x03, 0xe8})
+	msg, err := New(r)
+	assert.Nil(t, err)
+
+	t.Logf("%+v", msg)
+
+	r = bytes.NewReader([]byte{0x20, 0x07, 0x52, 0x45, 0x30, 0x35, 0x42, 0x4b, 0x47, 0x00, 0x01, 0xe2, 0x40})
+
+	msg, err = New(r)
+	assert.Nil(t, err)
+
+	t.Logf("%+v", msg)
 }
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
