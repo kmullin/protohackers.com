231aa9cb919c3cac74565ad277d70934c3d055f0
Kevin Mullin <kevin@kpmullin.com>
2025-11-29 09:34:09 -0800

    simplify this its a single byte

--- 6/message/heartbeat.go
+++ 6/message/heartbeat.go
@@ -27,10 +27,5 @@ func (hb *WantHeartbeat) UnmarshalBinary(data []byte) error {
 type Heartbeat struct{}
 
 func (hb Heartbeat) MarshalBinary() ([]byte, error) {
-	var buf bytes.Buffer
-
-	if err := binary.Write(&buf, byteOrder, MsgTypeHeartbeat); err != nil {
-		return nil, err
-	}
-	return buf.Bytes(), nil
+	return []byte{MsgTypeHeartbeat}, nil
 }
